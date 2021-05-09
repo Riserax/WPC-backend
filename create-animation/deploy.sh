d03a3d6c5353ffd505f4bb5f0be0d9c5f5d339f7
KC3 <camil.kala@gmail.com>
2021-05-09 17:06:57 +0000

    add create-animation and example jsons

---
+++ create-animation/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+BUCKET_NAME=192426
+FUNCTION_NAME=192426__create-animation
+
+## build
+rm lambda.zip || true
+
+zip -r lambda.zip ./* \
+    --exclude 'lambda.zip' \
+    --exclude 'deploy.sh'
+
+## release
+aws s3 cp ./lambda.zip s3://${BUCKET_NAME}/code/${FUNCTION_NAME}/lambda.zip
+
+## update code
+aws lambda update-function-code --function-name ${FUNCTION_NAME} --s3-bucket ${BUCKET_NAME} --s3-key code/${FUNCTION_NAME}/lambda.zip --publish
